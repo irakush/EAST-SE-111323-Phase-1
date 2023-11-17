38833ce1409d53b3b55a8fcea20155a35f945623
Igor Rakush <irakush@aol.com>
2023-11-17 11:56:06 -0500

    add functionality

--- src/index.js
+++ src/index.js
@@ -1,114 +1,41 @@
-const foods = [
-    {
-        name: "Flatburger",
-        image: "./assets/food/flatburger.jpeg",
-        description: "Our signature Flatburger which is a super delicious cheeseburger that'll leave you wanting another Flatburger! It's too good to be true!"
-    },
-    {
-        name: "Maple Bacon Burger",
-        image: "./assets/food/maple-bacon-burger.jpeg",
-        description: "A great option for a breakfast burger or a great option for brunch if you're hungry enough to eat 2 of these burgers!"
-    },
-    {
-        name: "Mushroom Burger",
-        image: "./assets/food/mushroom-burger.webp",
-        description: "A mushroom burger with our exclusive Flatburger melted cheese!"
-    },
-    {
-        name: "Avocado Bun Burger",
-        image: "./assets/food/avocado-bun-burger.jpeg",
-        description: "A healthier take on our signature Flatburger but with avocado buns!"
-    },
-    {
-        name: "Ramen Burger",
-        image: "./assets/food/ramen-burger.jpeg",
-        description: "If you love ramen and burgers, what are you waiting for? Order our exclusive ramen burger now!"
-    },
-    {
-        name: "French Fries",
-        image: "./assets/food/french-fries.jpeg",
-        description: "The good old french fries made in Flatburger style!"
-    },
-    {
-        name: "Burrito",
-        image: "./assets/food/burrito.webp",
-        description: "A Flatburger style burrito with our exclusive Flatburger melted cheese and any meat of your choosing in addition to your choice of additional ingredients!"
-    },
-    {
-        name: "Taco",
-        image: "./assets/food/taco.jpeg",
-        description: "The Flatburger exclusive Taco! Get it while supplies last!"
-    },
-    {
-        name: "Hot Dog",
-        image: "./assets/food/hot-dog.jpeg",
-        description: "The Flatburger exclusive Hot Dog! Get it while supplies last!"
-    },
-    {
-        name: "Onion Rings",
-        image: "./assets/food/onion-rings.jpeg",
-        description: "The Flatburger exclusive Onion Rings! Get them while supplies last!"
-    }
-]
+
+// write your code here
+// Deliverable 1
+// const promiseDataResponse = fetch('http://localhost:3000/foods')
+// const promiseDataFoodToJson = promiseDataResponse.then(respones => respones.json)
+// const promiseDataFoodObject = promiseDataFood.then(food => console.log(food))
 
 const restaurantMenu = document.getElementById('restaurant-menu')
 
-foods.forEach(food => {
+fetch('http://localhost:3000/foods')
+.then(respones => respones.json())
+.then(foods => {
+  
+  foods.forEach(food => {
     addFoodImageToRestaurantMenu(food)
-})
+  })
 
-function displayFoodDetails(food) {
-    const foodDetailImage = document.querySelector('.detail-image')
-    foodDetailImage.src = food.image
-    const foodName = document.querySelector('.name') 
-    foodName.textContent = food.name
-    const foodDescriptionDisplay = document.querySelector('#description-display')
-    foodDescriptionDisplay.textContent = food.description
-}
+  showFoodDetails(foods[0])
+})
+.catch(() => console.error('Oops something goes wrong ...'))
 
 function addFoodImageToRestaurantMenu(food){
-    const foodImage = document.createElement('img')
-    foodImage.src = food.image
-    restaurantMenu.appendChild(foodImage)
-
-    // Add event listener
-    foodImage.addEventListener('click', () => {
-        displayFoodDetails(food)
-    })
+  const imgElement = document.createElement('img')
+  imgElement.addEventListener('click', test)
+  imgElement.src = food.image
+  restaurantMenu.appendChild(imgElement)
 }
 
-// const foodDetailImage = document.querySelector('.detail-image')
-// foodDetailImage.src = foods[0].image
+function showFoodDetails(food){
+  const detailImage = document.getElementsByClassName('detail-image')[0]
+  const detailName = document.getElementsByClassName('name')[0]
+  const detailDescription = document.getElementsByClassName('description-display')[0]
 
-// const foodName = document.querySelector('.name') 
-// foodName.textContent = foods[0].name
+  detailImage.src = food.image
+  detailName.textContent = food.name
+  detailDescription.textContent = food.description
+}
 
-// const foodDescriptionDisplay = document.querySelector('#description-display')
-// foodDescriptionDisplay.textContent = foods[0].description
-
-displayFoodDetails(foods[0])
-
-// Lecture code
-// foodDetailImage.addEventListener('click', () => {
-//     console.log('Clicked!')
-// })
-
-const newFoodForm = document.getElementById('new-food')
-newFoodForm.addEventListener('submit', (event) => {
-    // prevent form from refreshing when click Submit
-    event.preventDefault()
-    
-    const newNameInputElement = document.getElementById('new-name')
-    const newImageInputElement = document.getElementById('new-image')
-    const newDescriptionElement = document.getElementById('new-description')
-
-    const newFood = {
-        name: newNameInputElement.value,
-        image: newImageInputElement.value,
-        description: newDescriptionElement.value
-    }
-
-    addFoodImageToRestaurantMenu(newFood)
-
-    newFoodForm.reset()
-})
+function test(){
+  
+}
